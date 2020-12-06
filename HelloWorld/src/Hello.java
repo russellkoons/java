d41ee4d0bc45784d51e3d24167d8b166340bb1cb
russellkoons <rkoons@gmail.com>
2020-12-06 17:27:59 -0500

    myFirstNumber variable

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }
