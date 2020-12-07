b777b1105078c36c78ffae632419a883dd69982b
russellkoons <rkoons@gmail.com>
2020-12-07 06:22:31 -0500

    finished floats and doubles

--- 01 - First Steps/FloatAndDouble/src/com/company/Main.java
+++ 01 - First Steps/FloatAndDouble/src/com/company/Main.java
@@ -14,9 +14,23 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
+        int myIntValue = 5 / 3;
         // If you don't include an f or (float) here you get an error
-        float myFloatValue = (float) 5.25;
-        double myDoubleValue = 5.25d;
+        float myFloatValue = 5f / 3f;
+        // double is the standard floating point type
+        double myDoubleValue = 5.00 / 3.00; // Can also add d instead of .00
+        System.out.println("myIntValue = " + myIntValue); // returns 1
+        System.out.println("myFloatValue = " + myFloatValue); // returns 1.6666666
+        System.out.println("myDoubleValue = " + myDoubleValue); // returns 1.6666666666666667
+
+        // float and double challenge
+        double pounds = 200d;
+        double kilos = pounds * 0.45359237d;
+        System.out.println(kilos);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }
