e3dce3df6d2b9d246f0dafa7a8b2a25a1a0a4b5b
russellkoons <rkoons@gmail.com>
2020-12-10 06:20:18 -0500

    ternary operators

--- 01 - First Steps/OperatorsOperandsExpressions/src/com/company/Main.java
+++ 01 - First Steps/OperatorsOperandsExpressions/src/com/company/Main.java
@@ -87,8 +87,14 @@ public class Main {
         }
 
         boolean isCar = false;
-        if (isCar == true) {
+        if (isCar) {
             System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
         }
     }
 }
