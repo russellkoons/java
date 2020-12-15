cc159944f79e4f688dc7afa843841234d8e802b2
russellkoons <rkoons@gmail.com>
2020-12-15 06:28:16 -0500

    return

--- 02 - Java Tutorial/Methods/src/com/company/Main.java
+++ 02 - Java Tutorial/Methods/src/com/company/Main.java
@@ -8,26 +8,20 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore();
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(false, 10000, 8, 200);
     }
 
-    public static void calculateScore() {
-        boolean gameOver = true;
-        int score = 5000;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+        
+        return -1;
     }
 }
