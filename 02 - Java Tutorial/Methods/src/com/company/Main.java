d314601cf395bd7356cb486c05595364e461ce24
russellkoons <rkoons@gmail.com>
2020-12-15 06:06:46 -0500

    added starter code for methods

--- 02 - Java Tutorial/Methods/src/com/company/Main.java
+++ 02 - Java Tutorial/Methods/src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        // int savedFinalScore = finalScore; Doesn't work because finalScore is in code block
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore() {
+        
     }
 }
