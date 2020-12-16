5da0a6a7e5095f0e16bbac407d960d3b2d5db998
russellkoons <rkoons@gmail.com>
2020-12-16 06:28:57 -0500

    alternate challenge solution

--- 02 - Java Tutorial/Methods/src/com/company/Main.java
+++ 02 - Java Tutorial/Methods/src/com/company/Main.java
@@ -24,15 +24,26 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int score) {
+//        if (score > 1000) {
+//            return 1;
+//        } else if (score > 500) {
+//            return 2;
+//        } else if (score > 100) {
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+
+        int position = 4;
         if (score > 1000) {
-            return 1;
+            position = 1;
         } else if (score > 500) {
-            return 2;
+            position = 2;
         } else if (score > 100) {
-            return 3;
-        } else {
-            return 4;
+            position = 3;
         }
+
+        return position;
     }
 
     public static void displayHighScorePosition(String name, int position) {
