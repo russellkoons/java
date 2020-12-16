c61edb3f63b6326eee656098fbbaecc5cbd89a88
russellkoons <rkoons@gmail.com>
2020-12-16 06:10:52 -0500

    methods challenge

--- 02 - Java Tutorial/Methods/src/com/company/Main.java
+++ 02 - Java Tutorial/Methods/src/com/company/Main.java
@@ -12,6 +12,31 @@ public class Main {
         System.out.println("Your final score was " + highScore);
 
         calculateScore(false, 10000, 8, 200);
+
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Russell", position);
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("John", position);
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Steve", position);
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("Natalie", position);
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score > 1000) {
+            return 1;
+        } else if (score > 500 && score < 1000) {
+            return 2;
+        } else if (score > 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
