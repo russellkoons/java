1b88f398219bc0d13d10858f3b8fc6b6d53754aa
russellkoons <rkoons@gmail.com>
2020-12-17 06:13:28 -0500

    toMilesPerHour

--- 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
+++ 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
@@ -1,3 +1,12 @@
 public class SpeedConverter {
-    
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        long milesPerHour = Math.round(kilometersPerHour / 1.609);
+        return milesPerHour;
+    }
 }
