d26df3913891b0475426263d27ba20e02133e53e
russellkoons <rkoons@gmail.com>
2020-12-17 06:20:50 -0500

    fixed a couple printConversion errors

--- 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
+++ 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
@@ -12,8 +12,8 @@ public class SpeedConverter {
     public static void printConversion(double kilometersPerHour) {
         if (kilometersPerHour < 0) {
             System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + "km/h = " + toMilesPerHour(kilometersPerHour) + "mi/h");
         }
-
-        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
     }
 }
