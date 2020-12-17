305dbf826283ea00697cb0d7f1467ad535d064ed
russellkoons <rkoons@gmail.com>
2020-12-17 06:18:26 -0500

    printConversion

--- 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
+++ 02 - Java Tutorial/SpeedConverter/src/SpeedConverter.java
@@ -6,7 +6,14 @@ public class SpeedConverter {
             return -1;
         }
 
-        long milesPerHour = Math.round(kilometersPerHour / 1.609);
-        return milesPerHour;
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        }
+
+        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
     }
 }
