eb9a58068d88c74de5d4d6bb188d140b95963d84
russellkoons <rkoons@gmail.com>
2020-12-17 05:58:54 -0500

    added checknumber to main

--- 02 - Java Tutorial/FirstCodingExercise/src/com/company/Main.java
+++ 02 - Java Tutorial/FirstCodingExercise/src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        checkNumber(5);
+    }
 
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number > 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
     }
 }
--- 02 - Java Tutorial/FirstCodingExercise/src/com/company/PositiveNegativeZero.java
+++ 02 - Java Tutorial/FirstCodingExercise/src/com/company/PositiveNegativeZero.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class PositiveNegativeZero {
     public static void main(String[] args) {
-        
+        checkNumber(5);
     }
 
     public static void checkNumber(int number) {
