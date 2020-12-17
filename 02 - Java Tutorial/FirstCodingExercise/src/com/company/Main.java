e77c3ea15254532116e4badcd97da7b8dd151320
russellkoons <rkoons@gmail.com>
2020-12-17 05:59:47 -0500

    fixed error

--- 02 - Java Tutorial/FirstCodingExercise/src/com/company/Main.java
+++ 02 - Java Tutorial/FirstCodingExercise/src/com/company/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         checkNumber(5);
+        checkNumber(-2);
+        checkNumber(0);
     }
 
     public static void checkNumber(int number) {
         if (number > 0) {
             System.out.println("positive");
-        } else if (number > 0) {
+        } else if (number < 0) {
             System.out.println("negative");
         } else {
             System.out.println("zero");
