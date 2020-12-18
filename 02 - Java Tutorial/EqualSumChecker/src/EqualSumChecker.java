87adfb13de87c8420259bdf912ebae2195880d67
russellkoons <rkoons@gmail.com>
2020-12-18 06:35:12 -0500

    EqualSumChecker done

--- 02 - Java Tutorial/EqualSumChecker/src/EqualSumChecker.java
+++ 02 - Java Tutorial/EqualSumChecker/src/EqualSumChecker.java
@@ -1,2 +1,8 @@
 public class EqualSumChecker {
+    public static boolean hasEqualSum(int one, int two, int three) {
+        if (one + two == three) {
+            return true;
+        }
+        return false;
+    }
 }
