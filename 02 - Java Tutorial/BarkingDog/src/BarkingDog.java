79a485ff58f1cbae2da063d7015fc6f5532ea423
russellkoons <rkoons@gmail.com>
2020-12-18 06:02:42 -0500

    BarkingDog

---
+++ 02 - Java Tutorial/BarkingDog/src/BarkingDog.java
@@ -0,0 +1,15 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else if (isBarking) {
+            if (hourOfDay < 8 || hourOfDay > 22) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
