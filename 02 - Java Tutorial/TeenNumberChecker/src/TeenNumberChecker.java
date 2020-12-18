6a0240141c421e10f07305476f1c95b8655020fc
russellkoons <rkoons@gmail.com>
2020-12-18 06:40:49 -0500

    hasTeen

--- 02 - Java Tutorial/TeenNumberChecker/src/TeenNumberChecker.java
+++ 02 - Java Tutorial/TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,2 +1,15 @@
 public class TeenNumberChecker {
+    public static boolean hasTeen(int one, int two, int three) {
+        if (one >= 13 || one <= 19) {
+            return true;
+        } else if (two >= 13 || two <= 19) {
+            return true;
+        } else if (three >= 13 || three <= 19) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
 }
