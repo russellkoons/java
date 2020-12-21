aea45a13dcfd3e1c8d183ec08e00d18af0c45f98
russellkoons <rkoons@gmail.com>
2020-12-21 05:57:27 -0500

    more calculateScore functions

--- 02 - Java Tutorial/MethodOverloading/src/com/company/Main.java
+++ 02 - Java Tutorial/MethodOverloading/src/com/company/Main.java
@@ -3,11 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    calculateScore("Russell", 500);
+	    int newScore = calculateScore("Russell", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String name, int score) {
         System.out.println("Player " + name + " scored " + score + " points");
         return score * 1000;
+    }
+
+    // you can create another method with the same name if you change the number of parameters, it's based on parameters and data types
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name or score");
+        return 0;
     }
 }
