817043154e18a717c1089e262e6ae7b38b1cd010
russellkoons <rkoons@gmail.com>
2020-12-21 06:31:49 -0500

    getDurationString

--- 02 - Java Tutorial/SecondAndMinutesChallenge/src/Main.java
+++ 02 - Java Tutorial/SecondAndMinutesChallenge/src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(getDurationString(65, 45));
     }
 
     private static String getDurationString(long minutes, long seconds) {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            System.out.println("Invalid value");
+            return "Invalid value";
         }
+
+        long hours = (minutes / 60);
+        long remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return "Invalid value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
         
     }
 }
