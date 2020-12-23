e5255bb9f64815c5ac633fd479427b1a4aaf5ce4
russellkoons <rkoons@gmail.com>
2020-12-23 05:33:43 -0500

    isLeapYear added

--- 03 - Control Flow Statements/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ 03 - Control Flow Statements/NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -1,3 +1,19 @@
 public class NumberOfDaysInMonth {
-    
+    public static boolean isLeapYear(int year) {
+        if (year <= 0 || year > 9999) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
