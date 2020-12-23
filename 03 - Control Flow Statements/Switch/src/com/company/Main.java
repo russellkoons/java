69cd5ff01d85ebf1a70e7fdadb6340bdd6c47873
russellkoons <rkoons@gmail.com>
2020-12-23 04:53:12 -0500

    starting with switch

--- 03 - Control Flow Statements/Switch/src/com/company/Main.java
+++ 03 - Control Flow Statements/Switch/src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int value = 1;
+	    if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Value was not 1 or 2");
+        }
+
+	    int switchValue = 1;
+
+	    switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+        }
+
+        // more code here
     }
 }
