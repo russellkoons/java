14073d25e13ee7418a0f852eeaf1b7fd0900be31
russellkoons <rkoons@gmail.com>
2020-12-23 05:56:20 -0500

    backwards for loop challenge

--- 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
+++ 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
@@ -11,6 +11,10 @@ public class Main {
         for(int i = 2; i < 9; i++) {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.1f", calculateInterest(10000.0, i)));
         }
+
+        for(int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.1f", calculateInterest(10000.0, i)));
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
