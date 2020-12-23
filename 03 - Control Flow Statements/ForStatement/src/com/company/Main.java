a143b934005a815f9cc31814009d7c56d8174155
russellkoons <rkoons@gmail.com>
2020-12-23 05:44:55 -0500

    non-for answer

--- 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
+++ 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
     }
 }
