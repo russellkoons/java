4ea58c8813c9f3f942d1de465e675a18f9edc42b
russellkoons <rkoons@gmail.com>
2020-12-23 05:59:50 -0500

    added isPrime

--- 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
+++ 03 - Control Flow Statements/ForStatement/src/com/company/Main.java
@@ -17,6 +17,20 @@ public class Main {
         }
     }
 
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n/2; i++){
+            if (n % 1 == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
     }
