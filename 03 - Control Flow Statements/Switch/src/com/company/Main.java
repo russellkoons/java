8dbd2d6b520095ace2606f71e5a167101bad2951
russellkoons <rkoons@gmail.com>
2020-12-23 04:54:53 -0500

    case and default

--- 03 - Control Flow Statements/Switch/src/com/company/Main.java
+++ 03 - Control Flow Statements/Switch/src/com/company/Main.java
@@ -12,11 +12,20 @@ public class Main {
             System.out.println("Value was not 1 or 2");
         }
 
-	    int switchValue = 1;
+	    int switchValue = 3;
 
 	    switch (switchValue) {
             case 1:
                 System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            // This is basically else
+            default:
+                System.out.println("Was not 1 or 2");
                 break;
         }
 
