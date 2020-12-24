961bc2ed1f0a32e7219b746c18660298ed074f22
russellkoons <rkoons@gmail.com>
2020-12-24 06:27:30 -0500

    sumOdd challenge complete

--- 03 - Control Flow Statements/SumOddRange/src/SumOddRange.java
+++ 03 - Control Flow Statements/SumOddRange/src/SumOddRange.java
@@ -10,4 +10,20 @@ public class SumOddRange {
 
         return true;
     }
+
+    public static int sumOdd(int start, int end) {
+        if (end < start || start < 0 || end < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
 }
