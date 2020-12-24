fe9d283f9bdebec35d785fe7fa1370db80dae67c
russellkoons <rkoons@gmail.com>
2020-12-24 06:23:54 -0500

    isOdd

--- 03 - Control Flow Statements/SumOddRange/src/SumOddRange.java
+++ 03 - Control Flow Statements/SumOddRange/src/SumOddRange.java
@@ -1,3 +1,13 @@
 public class SumOddRange {
-    
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        if (number % 2 == 0) {
+            return false;
+        }
+
+        return true;
+    }
 }
