9ad540dfc16bf8e1ff7cd7ec7394e85929f576eb
russellkoons <rkoons@gmail.com>
2020-12-26 08:09:44 -0500

    first while loop

--- 03 - Control Flow Statements/WhileDoWhile/src/com/company/Main.java
+++ 03 - Control Flow Statements/WhileDoWhile/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    
+	    int count = 1;
+
+	    while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+	    for (int i = 1; i < 6; i++) {
+            System.out.println("Count value is " + i);
+        }
     }
 }
