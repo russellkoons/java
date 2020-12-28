31dc03ebdeb78be8df9578b4709447d84d08caf1
russellkoons <rkoons@gmail.com>
2020-12-28 06:22:56 -0500

    hasSameLastDigit

--- 03 - Control Flow Statements/LastDigitChecker/src/LastDigitChecker.java
+++ 03 - Control Flow Statements/LastDigitChecker/src/LastDigitChecker.java
@@ -1,2 +1,13 @@
 public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int x, int y, int z) {
+        if (x < 10 || x > 1000 || y < 10 || y > 1000 || z < 10 || z > 1000) {
+            return false;
+        }
+
+        if (x % 10 == y % 10 || x % 10 == z % 10 || y % 10 == z % 10) {
+            return true;
+        }
+
+        return false;
+    }
 }
