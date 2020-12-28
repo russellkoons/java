a82713c3cbdbda4183ec6f27b2d34fa50085f839
russellkoons <rkoons@gmail.com>
2020-12-28 05:59:05 -0500

    isPalindrome challenge complete

--- 03 - Control Flow Statements/NumberPalindrome/src/NumberPalindrome.java
+++ 03 - Control Flow Statements/NumberPalindrome/src/NumberPalindrome.java
@@ -1,2 +1,22 @@
 public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            number *= -1;
+        }
+
+        int reverse = 0;
+        int original = number;
+        while (number != 0) {
+            reverse *= 10;
+            int lastDigit = number % 10;
+            reverse += lastDigit;
+            number /= 10;
+        }
+
+        if (original == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
