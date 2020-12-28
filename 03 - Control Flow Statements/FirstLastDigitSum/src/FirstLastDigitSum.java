635fe376ca6573f414167df904fc694e73f6b0e7
russellkoons <rkoons@gmail.com>
2020-12-28 06:04:46 -0500

    sumFirstAndLastDigit challenge complete

--- 03 - Control Flow Statements/FirstLastDigitSum/src/FirstLastDigitSum.java
+++ 03 - Control Flow Statements/FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -1,3 +1,25 @@
 public class FirstLastDigitSum {
-    
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int original = number;
+        int first = 0;
+        int last = 0;
+
+        while (number != 0) {
+            if (original == number) {
+                first = number % 10;
+            }
+
+            if (number / 10 == 0) {
+                last = number;
+            }
+
+            number /= 10;
+        }
+
+        return first + last;
+    }
 }
