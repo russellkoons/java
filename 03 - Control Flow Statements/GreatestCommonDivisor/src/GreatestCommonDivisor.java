3d882edc9ffa777c1ef755b1a4925c8a10d8901e
russellkoons <rkoons@gmail.com>
2020-12-29 06:03:54 -0500

    GreatestCommonDivisor challenge complete

--- 03 - Control Flow Statements/GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ 03 - Control Flow Statements/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,2 +1,17 @@
 public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int x, int y) {
+        if (x < 10 || y < 10) {
+            return -1;
+        }
+
+        int divisor = 0;
+
+        for (int i = 1; i < x; i++) {
+            if (x % i == 0 && y % i == 0) {
+                divisor = i;
+            }
+        }
+
+        return divisor;
+    }
 }
