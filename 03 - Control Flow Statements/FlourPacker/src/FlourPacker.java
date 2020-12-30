c696f93bcc419b4dec8ed4f1ee10fb0f92ea2e82
russellkoons <rkoons@gmail.com>
2020-12-30 06:03:06 -0500

    flour pack problem complete

--- 03 - Control Flow Statements/FlourPacker/src/FlourPacker.java
+++ 03 - Control Flow Statements/FlourPacker/src/FlourPacker.java
@@ -1,2 +1,34 @@
 public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        int sum = 0;
+
+        if (bigCount == 0) {
+            for (int i = 0; i < smallCount; i++) {
+                sum++;
+                if (sum == goal) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < bigCount; i++) {
+                sum += 5;
+                if (sum == goal) {
+                    return true;
+                }
+                for (int j = 0; j < smallCount; j++) {
+                    sum++;
+                    if (sum == goal) {
+                        return true;
+                    }
+                }
+                sum -= smallCount;
+            }
+        }
+
+        return false;
+    }
 }
