a1aec4556f6416abb9fe85dd25351955684b268b
russellkoons <rkoons@gmail.com>
2020-12-30 06:10:20 -0500

    getLargestPrime complete

--- 03 - Control Flow Statements/LargestPrime/src/LargestPrime.java
+++ 03 - Control Flow Statements/LargestPrime/src/LargestPrime.java
@@ -1,2 +1,19 @@
 public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+
+        int divisor = 2;
+        while (divisor < number) {
+            if (number % divisor != 0) {
+                divisor++;
+            } else {
+                number = number / divisor;
+                divisor = 2;
+            }
+        }
+
+        return number;
+    }
 }
