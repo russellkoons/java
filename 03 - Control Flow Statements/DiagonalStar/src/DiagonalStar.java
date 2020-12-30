e631ddc3205b38b06a2827ae1402cea8d39e9f75
russellkoons <rkoons@gmail.com>
2020-12-30 06:28:34 -0500

    printSquareStar challenge complete

--- 03 - Control Flow Statements/DiagonalStar/src/DiagonalStar.java
+++ 03 - Control Flow Statements/DiagonalStar/src/DiagonalStar.java
@@ -1,2 +1,21 @@
 public class DiagonalStar {
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        String line = "";
+        for (int i = 0; i < number; i++) {
+            line = "";
+            for (int j = 0; j < number; j++) {
+                if (i == 0 || i == number - 1 || j == 0 || j == number - 1 || i == j || number - i - 1 == j) {
+                    line += "*";
+                } else {
+                    line += " ";
+                }
+            }
+            System.out.println(line);
+        }
+    }
 }
