52d9916a68c2075936150fb69e70fbeb5e5ea288
russellkoons <rkoons@gmail.com>
2020-12-30 05:43:24 -0500

    parseInt

--- 03 - Control Flow Statements/ParsingValueFromString/src/com/company/Main.java
+++ 03 - Control Flow Statements/ParsingValueFromString/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        String numberAsString = "2018";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
     }
 }
