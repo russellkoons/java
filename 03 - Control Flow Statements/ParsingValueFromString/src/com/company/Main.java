9d0053c25f0b1a458d96b2f79b73a5f9765f25c8
russellkoons <rkoons@gmail.com>
2020-12-30 05:47:31 -0500

    adding ints to strings

--- 03 - Control Flow Statements/ParsingValueFromString/src/com/company/Main.java
+++ 03 - Control Flow Statements/ParsingValueFromString/src/com/company/Main.java
@@ -7,5 +7,12 @@ public class Main {
         System.out.println("numberAsString = " + numberAsString);
 
         int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        // First concatenates since it is a string, second adds it since it is an int
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println(numberAsString + " " + number);
     }
 }
