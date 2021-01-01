566a11667da5411ce296b94d5c2c8599f96278dc
russellkoons <rkoons@gmail.com>
2021-01-01 08:12:08 -0500

    added main to DiagonalStar

--- 03 - Control Flow Statements/DiagonalStar/src/DiagonalStar.java
+++ 03 - Control Flow Statements/DiagonalStar/src/DiagonalStar.java
@@ -5,9 +5,8 @@ public class DiagonalStar {
             return;
         }
 
-        String line = "";
         for (int i = 0; i < number; i++) {
-            line = "";
+            String line = "";
             for (int j = 0; j < number; j++) {
                 if (i == 0 || i == number - 1 || j == 0 || j == number - 1 || i == j || number - i - 1 == j) {
                     line += "*";
