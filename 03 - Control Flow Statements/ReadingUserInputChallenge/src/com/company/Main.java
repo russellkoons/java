aefc0411c045947bb43f82cb569a7b90423f3e38
russellkoons <rkoons@gmail.com>
2021-01-06 06:24:45 -0500

    challenge begun

--- 03 - Control Flow Statements/ReadingUserInputChallenge/src/com/company/Main.java
+++ 03 - Control Flow Statements/ReadingUserInputChallenge/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (counter < 10) {
+            
+            counter++;
+        }
+
+        scanner.close();
     }
 }
