7b00192f91965b399e4f0453321b84575d3db911
russellkoons <rkoons@gmail.com>
2021-01-06 06:46:54 -0500

    MinMaxChallenge complete

--- 03 - Control Flow Statements/MixMaxChallenge/src/com/company/Main.java
+++ 03 - Control Flow Statements/MixMaxChallenge/src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int min = 0;
+        int max = 0;
+        boolean invalid = false;
+
+        while (true) {
+            System.out.println("Enter number:");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int n = scanner.nextInt();
+                if (min == 0 && max == 0) {
+                    min = n;
+                    max = n;
+                } else if (n < min) {
+                    min = n;
+                } else if (n > max) {
+                    max = n;
+                }
+            } else {
+                invalid = true;
+            }
+
+            if (invalid) {
+                break;
+            }
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
+        scanner.close();
     }
 }
