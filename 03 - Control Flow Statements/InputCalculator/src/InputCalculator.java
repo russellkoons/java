686529240b32b64ebebe11f937b46ca01d1a713c
russellkoons <rkoons@gmail.com>
2021-01-08 06:42:14 -0500

    InputCalculator done

--- 03 - Control Flow Statements/InputCalculator/src/InputCalculator.java
+++ 03 - Control Flow Statements/InputCalculator/src/InputCalculator.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int counter = 0;
+        long avg = 0L;
+        boolean invalid = false;
+
+        while (!invalid) {
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                counter++;
+                sum += scanner.nextInt();
+            } else {
+                invalid = true;
+            }
+
+            scanner.nextLine();
+        }
+
+        avg = Math.round((double) sum / counter);
+
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
+    }
 }
