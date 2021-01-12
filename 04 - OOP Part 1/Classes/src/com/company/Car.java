833ff6a0ebd95caeb9bb35910669ea02eb732e3f
russellkoons <rkoons@gmail.com>
2021-01-12 05:09:03 -0500

    added state components to class car

--- 04 - OOP Part 1/Classes/src/com/company/Car.java
+++ 04 - OOP Part 1/Classes/src/com/company/Car.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Car {
-    
+    private int doors; // state components
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
 }
