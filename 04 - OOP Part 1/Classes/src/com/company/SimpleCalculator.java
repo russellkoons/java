1f5877648c9240d4928b59f54e1a9692dd51f617
russellkoons <rkoons@gmail.com>
2021-01-12 05:32:35 -0500

    getFirstNumber and getSecondNumber

--- 04 - OOP Part 1/Classes/src/com/company/SimpleCalculator.java
+++ 04 - OOP Part 1/Classes/src/com/company/SimpleCalculator.java
@@ -1,2 +1,12 @@
 public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
 }
