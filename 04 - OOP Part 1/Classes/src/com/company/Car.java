764d11fba5a5b0c4e0d6fae0fa29920138bcf848
russellkoons <rkoons@gmail.com>
2021-01-12 05:27:18 -0500

    validModel check

--- 04 - OOP Part 1/Classes/src/com/company/Car.java
+++ 04 - OOP Part 1/Classes/src/com/company/Car.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Car {
     private int doors; // state components
     private int wheels; // private keeps classes outside of the class from accessing the variables
@@ -8,7 +10,12 @@ public class Car {
     private String color;
 
     public void setModel(String model) {
-        this.model = model;
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
     }
 
     public String getModel() {
--- 04 - OOP Part 1/Classes/src/com/company/Main.java
+++ 04 - OOP Part 1/Classes/src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
         Car porsche = new Car(); // you have to include new or it will crash
         Car holden = new Car();
         // porsche = null; this forces a crash
+        System.out.println("Model is " + porsche.getModel());
         porsche.setModel("Carrera");
-        System.out.println(porsche.getModel());
+        System.out.println("Model is " + porsche.getModel());
     }
 }
