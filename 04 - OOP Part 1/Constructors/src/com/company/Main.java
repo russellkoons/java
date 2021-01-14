d9a8a84008e7f26fd0e04359e419acd69e20c705
russellkoons <rkoons@gmail.com>
2021-01-14 06:17:20 -0500

    set stuffs

--- 04 - OOP Part 1/Constructors/src/com/company/Main.java
+++ 04 - OOP Part 1/Constructors/src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	    BankAccount myAccount = new BankAccount();
 
+	    myAccount.setNumber("42069");
+	    myAccount.setBalance(0.00);
+	    myAccount.setName("Russell");
+	    myAccount.setEmail("rkoons@gmail.com");
+	    myAccount.setPhone("(123) 456-7890");
+
 	    myAccount.withdraw(100.0);
 	    myAccount.deposit(50.0);
         myAccount.withdraw(100.0);
