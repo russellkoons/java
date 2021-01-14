fd6bea0887c57d7d20a96e8e548fc414bfc452bb
russellkoons <rkoons@gmail.com>
2021-01-14 06:15:48 -0500

    challenge tester

--- 04 - OOP Part 1/Constructors/src/com/company/Main.java
+++ 04 - OOP Part 1/Constructors/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    BankAccount myAccount = new BankAccount();
+
+	    myAccount.withdraw(100.0);
+	    myAccount.deposit(50.0);
+        myAccount.withdraw(100.0);
+        myAccount.deposit(51.0);
+        myAccount.withdraw(100.0);
+
     }
 }
