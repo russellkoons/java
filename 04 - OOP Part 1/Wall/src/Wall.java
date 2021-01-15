79a6c9cbfc17941375246c8cdf16a91f1e64c21b
russellkoons <rkoons@gmail.com>
2021-01-15 05:44:00 -0500

    Wall constructors

--- 04 - OOP Part 1/Wall/src/Wall.java
+++ 04 - OOP Part 1/Wall/src/Wall.java
@@ -1,3 +1,21 @@
 public class Wall {
-    
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+
+        if (this.width < 0) {
+            this.width = 0;
+        }
+
+        if (this.height < 0) {
+            this.height = 0;
+        }
+    }
 }
