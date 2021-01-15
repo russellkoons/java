a909ef8146552ac52f8683d911c876e71e4b729a
russellkoons <rkoons@gmail.com>
2021-01-15 05:45:53 -0500

    Wall getters and setters

--- 04 - OOP Part 1/Wall/src/Wall.java
+++ 04 - OOP Part 1/Wall/src/Wall.java
@@ -7,15 +7,31 @@ public class Wall {
     }
 
     public Wall(double width, double height) {
-        this.width = width;
-        this.height = height;
+        setWidth(width);
+        setHeight(height);
+    }
 
-        if (this.width < 0) {
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
             this.width = 0;
+        } else {
+            this.width = width;
         }
+    }
 
-        if (this.height < 0) {
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
             this.height = 0;
+        } else {
+            this.height = height;
         }
     }
 }
