4487a8dfdf325fb7382b6b8bb06d922bb1f801bc
russellkoons <rkoons@gmail.com>
2021-01-15 05:49:32 -0500

    Point constructors

--- 04 - OOP Part 1/Point/src/Point.java
+++ 04 - OOP Part 1/Point/src/Point.java
@@ -1,3 +1,13 @@
 public class Point {
-    
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
