632c49e14020064b9f4dcf081b442c6e464d1365
russellkoons <rkoons@gmail.com>
2021-01-16 07:41:44 -0500

    Carpet constructor

--- 04 - OOP Part 1/Point/src/Carpet.java
+++ 04 - OOP Part 1/Point/src/Carpet.java
@@ -1,3 +1,11 @@
 public class Carpet {
-    
+    private double cost;
+
+    public Carpet(double cost) {
+        if (cost < 0) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
+        }
+    }
 }
