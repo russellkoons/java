76a245af0d65409ce139907b8108e5f81b8ccbc2
russellkoons <rkoons@gmail.com>
2021-01-16 07:44:25 -0500

    Calculator constructor

--- 04 - OOP Part 1/Point/src/Calculator.java
+++ 04 - OOP Part 1/Point/src/Calculator.java
@@ -1,2 +1,11 @@
 public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    
 }
