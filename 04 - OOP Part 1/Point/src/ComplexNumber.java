8a2502a03a9c22b130ef54becf20e8ab9eb7cece
russellkoons <rkoons@gmail.com>
2021-01-16 07:48:22 -0500

    ComplexNumber constructor

--- 04 - OOP Part 1/Point/src/ComplexNumber.java
+++ 04 - OOP Part 1/Point/src/ComplexNumber.java
@@ -1,2 +1,9 @@
 public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
 }
