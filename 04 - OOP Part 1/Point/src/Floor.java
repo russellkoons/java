bf18ba11950459563ef99b54a6c450b1d0cc7829
russellkoons <rkoons@gmail.com>
2021-01-16 07:39:06 -0500

    Floor Constructor

--- 04 - OOP Part 1/Point/src/Floor.java
+++ 04 - OOP Part 1/Point/src/Floor.java
@@ -1,3 +1,17 @@
 public class Floor {
-    
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width;
+        this.length = length;
+
+        if (this.width < 0) {
+            this.width = 0;
+        }
+
+        if (this.length < 0) {
+            this.length = 0;
+        }
+    }
 }
