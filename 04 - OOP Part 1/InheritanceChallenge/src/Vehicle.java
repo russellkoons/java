dbb3fb75e1ccbe5bcf06b6d8a20dbd66e2806925
russellkoons <rkoons@gmail.com>
2021-01-20 06:12:22 -0500

    Vehicle starter code

--- 04 - OOP Part 1/InheritanceChallenge/src/Vehicle.java
+++ 04 - OOP Part 1/InheritanceChallenge/src/Vehicle.java
@@ -1,2 +1,12 @@
 public class Vehicle {
+    private String name;
+    private String size;
+
+    private int velocity;
+    private int direction;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+    }
 }
