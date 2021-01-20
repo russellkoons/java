517512f2f11e7839ec0f450193be2e5ddb2e6e84
russellkoons <rkoons@gmail.com>
2021-01-20 06:26:31 -0500

    Outlander starter code

--- 04 - OOP Part 1/InheritanceChallenge/src/com/company/Outlander.java
+++ 04 - OOP Part 1/InheritanceChallenge/src/com/company/Outlander.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Outlander extends Car {
-    
+    private int roadServiceMonths;
+
+    public Outlander(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadServiceMonths) {
+        super("Outlander", "4WD", wheels, doors, gears, isManual);
+        this.roadServiceMonths = roadServiceMonths;
+    }
 }
