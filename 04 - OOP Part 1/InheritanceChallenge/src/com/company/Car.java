87bc9c3fa8291b10910d1633d94a982e493523ee
russellkoons <rkoons@gmail.com>
2021-01-20 06:20:07 -0500

    changeGear

--- 04 - OOP Part 1/InheritanceChallenge/src/com/company/Car.java
+++ 04 - OOP Part 1/InheritanceChallenge/src/com/company/Car.java
@@ -8,12 +8,17 @@ public class Car extends Vehicle{
 
     private int currentGear;
 
-    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
         super(name, size);
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
         this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + currentGear + " gear");
     }
 }
