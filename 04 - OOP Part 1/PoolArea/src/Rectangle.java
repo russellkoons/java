c3d409d2523b21a011c71b278277bb7d5d27336c
russellkoons <rkoons@gmail.com>
2021-01-22 06:24:07 -0500

    Rectangle starter

--- 04 - OOP Part 1/PoolArea/src/Rectangle.java
+++ 04 - OOP Part 1/PoolArea/src/Rectangle.java
@@ -1,2 +1,17 @@
 public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+
+        if (width < 0) {
+            this.width = 0;
+        }
+
+        if (length < 0) {
+            this.length = 0;
+        }
+    }
 }
