238ed124658aa5bbd6ae909f50e25c8f92c81d47
russellkoons <rkoons@gmail.com>
2021-01-22 06:17:12 -0500

    Circle starter code

--- 04 - OOP Part 1/InheritanceChallenge/src/com/company/Circle.java
+++ 04 - OOP Part 1/InheritanceChallenge/src/com/company/Circle.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
 }
