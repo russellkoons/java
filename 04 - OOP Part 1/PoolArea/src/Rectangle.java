be1ce7d9cbc6898e1e671ae2ed5db947b91b072d
russellkoons <rkoons@gmail.com>
2021-01-22 06:24:46 -0500

    getArea

--- 04 - OOP Part 1/PoolArea/src/Rectangle.java
+++ 04 - OOP Part 1/PoolArea/src/Rectangle.java
@@ -14,4 +14,16 @@ public class Rectangle {
             this.length = 0;
         }
     }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
 }
