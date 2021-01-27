f1c71a6c3d4021869c7cb840bc58dc5a80c8e8a2
russellkoons <rkoons@gmail.com>
2021-01-27 06:20:56 -0500

    added classes to Main

--- 05 - OOP Part 2/Composition/src/com/company/Main.java
+++ 05 - OOP Part 2/Composition/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+	    Case theCase = new Case("220B", "Dell", "240", dimensions);
+	    Monitor theMonitor = new Monitor("27 inch", "Acer", 27, new Resolution(2540, 1440));
+	    Motherboard motherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+	    PC thePC = new PC(theCase, theMonitor, motherboard);
     }
 }
