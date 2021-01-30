11811e3b127b9dd25484d9a5fe26e14519d1be2a
russellkoons <rkoons@gmail.com>
2021-01-30 07:26:33 -0500

    Printer constructor

--- 05 - OOP Part 2/Encapsulation/src/com/company/Printer.java
+++ 05 - OOP Part 2/Encapsulation/src/com/company/Printer.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
 }
