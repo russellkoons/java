28726cbec5c9dd3f56df905825a4b2d1d6158127
russellkoons <rkoons@gmail.com>
2021-01-30 07:12:27 -0500

    EnhancedPlayer and commented out code

--- 05 - OOP Part 2/Encapsulation/src/com/company/Main.java
+++ 05 - OOP Part 2/Encapsulation/src/com/company/Main.java
@@ -3,17 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Player player = new Player();
-        player.name = "Russell";
-        player.health = 20;
-        player.weapon = "Bat";
-
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-
-        damage = 11;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
+//        Player player = new Player();
+//        player.name = "Russell";
+//        player.health = 20;
+//        player.weapon = "Bat";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
     }
 }
--- 05 - OOP Part 2/Encapsulation/src/com/company/Player.java
+++ 05 - OOP Part 2/Encapsulation/src/com/company/Player.java
@@ -1,19 +1,19 @@
 package com.company;
 
 public class Player {
-    public String name;
-    public int health;
-    public String weapon;
-
-    public void loseHealth(int damage) {
-        this.health = this.health - damage;
-        if(this.health <= 0) {
-            System.out.println("Player knocked out");
-            // Reduce number of remaining lives
-        }
-    }
-
-    public int healthRemaining() {
-        return this.health;
-    }
+//    public String name;
+//    public int health;
+//    public String weapon;
+//
+//    public void loseHealth(int damage) {
+//        this.health = this.health - damage;
+//        if(this.health <= 0) {
+//            System.out.println("Player knocked out");
+//            // Reduce number of remaining lives
+//        }
+//    }
+//
+//    public int healthRemaining() {
+//        return this.health;
+//    }
 }
