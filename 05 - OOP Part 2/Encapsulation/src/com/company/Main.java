144df947fc9122e43475ce38beb4d61662ee8808
russellkoons <rkoons@gmail.com>
2021-01-30 07:05:41 -0500

    adding to main

--- 05 - OOP Part 2/Encapsulation/src/com/company/Main.java
+++ 05 - OOP Part 2/Encapsulation/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Player player = new Player();
+        player.name = "Russell";
+        player.health = 20;
+        player.weapon = "Bat";
     }
 }
