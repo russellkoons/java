cf8b14ff43f2f0c38c37f9e275f11960ab8edcfc
russellkoons <rkoons@gmail.com>
2021-01-30 07:04:03 -0500

    loseHealth

--- 05 - OOP Part 2/Encapsulation/src/com/company/Player.java
+++ 05 - OOP Part 2/Encapsulation/src/com/company/Player.java
@@ -2,6 +2,14 @@ package com.company;
 
 public class Player {
     public String name;
-    public String health;
+    public int health;
     public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if(this.health <= 0) {
+            System.out.println("Player knocked out");
+            // Reduce number of remaining lives
+        }
+    }
 }
