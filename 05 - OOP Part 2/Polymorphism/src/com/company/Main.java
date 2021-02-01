1ae37a1bec31cdf6269b5bac3a4ee2d2b27d10e5
russellkoons <rkoons@gmail.com>
2021-02-01 05:55:19 -0500

    Movie

--- 05 - OOP Part 2/Polymorphism/src/com/company/Main.java
+++ 05 - OOP Part 2/Polymorphism/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	    
+
     }
 }
