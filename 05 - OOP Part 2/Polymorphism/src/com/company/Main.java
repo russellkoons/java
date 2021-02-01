19e47be51ae34957815b0da17b7feadcd2bc8e22
russellkoons <rkoons@gmail.com>
2021-02-01 06:07:49 -0500

    RandomMovie

--- 05 - OOP Part 2/Polymorphism/src/com/company/Main.java
+++ 05 - OOP Part 2/Polymorphism/src/com/company/Main.java
@@ -60,11 +60,36 @@ class ForgettableMovie extends Movie {
     public ForgettableMovie() {
         super("Forgettable");
     }
+
+    // No plot method
 }
 
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static Movie randomMovie() {
+        int random = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated: " + random);
+        switch (random) {
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new ForgettableMovie();
+        }
+
+        return null;
     }
 }
