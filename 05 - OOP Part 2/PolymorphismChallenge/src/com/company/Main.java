ec2c2ecabd3c78684534ba32e9ed8ee89c467f5f
russellkoons <rkoons@gmail.com>
2021-02-02 06:21:40 -0500

    Car

--- 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
+++ 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
@@ -1,5 +1,31 @@
 package com.company;
 
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(boolean engine, int cylinders) {
+        this.engine = engine;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
