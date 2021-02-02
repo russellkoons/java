08b4dd952217b5e1bdbdde94907ebb611994de39
russellkoons <rkoons@gmail.com>
2021-02-02 06:23:43 -0500

    startEngine

--- 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
+++ 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
@@ -1,28 +1,28 @@
 package com.company;
 
-public class Car {
+class Car {
     private boolean engine;
     private int cylinders;
+    private String name;
     private int wheels;
 
-    public Car(boolean engine, int cylinders) {
-        this.engine = engine;
+    public Car(boolean engine, int cylinders, String name) {
+        this.engine = true;
         this.cylinders = cylinders;
+        this.name = name;
         this.wheels = 4;
     }
 
-
-
-    public boolean isEngine() {
-        return engine;
+    public String startEngine() {
+        return "Car -> start engine"
     }
 
     public int getCylinders() {
         return cylinders;
     }
 
-    public int getWheels() {
-        return wheels;
+    public String getName() {
+        return name;
     }
 }
 
