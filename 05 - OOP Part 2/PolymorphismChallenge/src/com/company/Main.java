a52647c2e94ce81372daa6002e2621cceb2679dd
russellkoons <rkoons@gmail.com>
2021-02-02 06:24:49 -0500

    accelerate and brake

--- 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
+++ 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
@@ -14,7 +14,15 @@ class Car {
     }
 
     public String startEngine() {
-        return "Car -> start engine"
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
     }
 
     public int getCylinders() {
