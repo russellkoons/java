209b44cac2db7f109a4f5c558c2adb429e22ef4f
russellkoons <rkoons@gmail.com>
2021-02-02 06:30:35 -0500

    Ford

--- 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
+++ 05 - OOP Part 2/PolymorphismChallenge/src/com/company/Main.java
@@ -58,6 +58,16 @@ class Mitsubishi extends Car {
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        
     }
 }
