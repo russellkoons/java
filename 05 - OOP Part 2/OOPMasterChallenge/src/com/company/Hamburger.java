cec2b2fa337f1b5895b73b8c1e2e8bcd35c8ddda
russellkoons <rkoons@gmail.com>
2021-02-03 06:22:21 -0500

    Souts

--- 05 - OOP Part 2/OOPMasterChallenge/src/com/company/Hamburger.java
+++ 05 - OOP Part 2/OOPMasterChallenge/src/com/company/Hamburger.java
@@ -20,26 +20,37 @@ public class Hamburger {
         this.meat = meat;
         this.price = price;
         this.breadRollType = breadRollType;
+        if (this.name == "Basic") {
+            System.out.println("Basic hamburger on a " + breadRollType + " roll with " + meat + ", price is " + price);
+        } else if (this.name == "Deluxe") {
+            System.out.println("Deluxe hamburger on a Brioche roll with Angus. price is 14.54");
+            System.out.println("Added Chips for an extra 2.75");
+            System.out.println("Added Drink for an extra 1.81");
+        }
     }
 
     public void addHamburgerAddition1(String name, double price) {
         this.addition1Name = name;
         this.addition1Price = price;
+        System.out.println("Added " + name + " for an extra " + price);
     }
 
     public void addHamburgerAddition2(String name, double price) {
         this.addition2Name = name;
         this.addition2Price = price;
+        System.out.println("Added " + name + " for an extra " + price);
     }
 
     public void addHamburgerAddition3(String name, double price) {
         this.addition3Name = name;
         this.addition3Price = price;
+        System.out.println("Added " + name + " for an extra " + price);
     }
 
     public void addHamburgerAddition4(String name, double price) {
         this.addition4Name = name;
         this.addition4Price = price;
+        System.out.println("Added " + name + " for an extra " + price);
     }
 
     public double itemizeHamburger() {
