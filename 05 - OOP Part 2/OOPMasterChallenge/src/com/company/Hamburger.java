33b9e6d5696c0b89309b67286a58088c3faa8ebc
russellkoons <rkoons@gmail.com>
2021-02-03 06:08:00 -0500

    Hamburger addition methods

--- 05 - OOP Part 2/OOPMasterChallenge/src/com/company/Hamburger.java
+++ 05 - OOP Part 2/OOPMasterChallenge/src/com/company/Hamburger.java
@@ -21,4 +21,24 @@ public class Hamburger {
         this.price = price;
         this.breadRollType = breadRollType;
     }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
 }
