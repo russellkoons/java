77f170e0d00abb4a1b33e7ace4c31ddd95aae77c
russellkoons <rkoons@gmail.com>
2021-02-04 06:24:57 -0500

    created my first array in java

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/Arrays/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/Arrays/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] myArray = new int[10]; // int[] says it's an array, int[10] says it has 10 slots
+        myArray[5] = 50; // This adds 50 to the 6th position (start at 0)
+
     }
 }
