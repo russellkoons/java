b4c49011d85e91223c1d00a6587deeea0dab8691
russellkoons <rkoons@gmail.com>
2021-02-08 05:48:22 -0500

    getIntegers

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ArraysChallenge/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ArraysChallenge/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+	    int[] myInts = getIntegers(5);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " numbers:\r");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
     }
 }
