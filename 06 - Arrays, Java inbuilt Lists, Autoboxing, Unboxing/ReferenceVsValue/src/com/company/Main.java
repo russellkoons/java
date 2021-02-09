62a0b0d5c63c3c0e3c92bf930799dee3e21a6330
russellkoons <rkoons@gmail.com>
2021-02-09 05:39:32 -0500

    different types

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ReferenceVsValue/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ReferenceVsValue/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myInt = 10;
+	    int anotherInt = myInt;
+
+        System.out.println("myInt = " + myInt);
+        System.out.println("anotherInt = " + anotherInt);
+
+        anotherInt++;
+
+        System.out.println("myInt = " + myInt);
+        System.out.println("anotherInt = " + anotherInt);
+
+        int[] myIntArray = new int[5];
     }
 }
