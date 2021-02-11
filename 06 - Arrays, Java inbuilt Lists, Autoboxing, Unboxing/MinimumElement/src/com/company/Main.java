d155b7d40838d4e2ada28332452553756ccd4f8d
russellkoons <rkoons@gmail.com>
2021-02-11 07:09:39 -0500

    reverse2 for loop

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
@@ -8,5 +8,10 @@ public class Main {
 
     private static void reverse2(int[] array) {
         int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+        }
     }
 }
