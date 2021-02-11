b6f39353d8870905270d8252df2edc4a3a89acce
russellkoons <rkoons@gmail.com>
2021-02-11 07:12:29 -0500

    checking code in main

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] array = {1, 5, 3, 7, 11, 9, 15};
 
+        System.out.println(Arrays.toString(array));
+        reverse2(array);
+        System.out.println(Arrays.toString(array));
     }
 
     private static void reverse2(int[] array) {
