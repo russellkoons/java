01559771f06a4a6dec15a0adde21a86b60bb271d
russellkoons <rkoons@gmail.com>
2021-02-11 07:05:10 -0500

    shortened the reverse code

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/ReverseArray.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/ReverseArray.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 
 public class ReverseArray {
     private static void reverse(int[] array) {
-        System.out.println("Array = " + Arrays.toString(array));
-
         int[] reversed = new int[array.length];
 
         for (int i = 0; i < array.length; i++) {
-            int hold = array[i];
-            reversed[array.length - 1 - i] = hold;
+            reversed[array.length - 1 - i] = array[i];
         }
 
+        System.out.println("Array = " + Arrays.toString(array));
         System.out.println("Reversed array = " + Arrays.toString(reversed));
     }
 }
