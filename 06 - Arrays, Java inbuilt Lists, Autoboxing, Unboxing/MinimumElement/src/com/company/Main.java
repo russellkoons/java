a01978e036fb22d57b0fd8c8c825afbfc3d3d7c0
russellkoons <rkoons@gmail.com>
2021-02-11 07:10:34 -0500

    maxIndex

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
         for (int i = 0; i < halfLength; i++) {
             int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
         }
     }
 }
