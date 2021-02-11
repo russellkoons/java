39f1318149b922af47813fcc0f9248eb56b48d99
russellkoons <rkoons@gmail.com>
2021-02-11 07:04:01 -0500

    reverse()

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/ReverseArray.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/MinimumElement/src/com/company/ReverseArray.java
@@ -1,5 +1,18 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ReverseArray {
-    
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+
+        int[] reversed = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            int hold = array[i];
+            reversed[array.length - 1 - i] = hold;
+        }
+
+        System.out.println("Reversed array = " + Arrays.toString(reversed));
+    }
 }
