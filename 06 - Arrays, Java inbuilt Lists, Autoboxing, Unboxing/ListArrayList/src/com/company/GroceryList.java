056a546f3dc54a132c42850793560441e848d87f
russellkoons <rkoons@gmail.com>
2021-02-12 06:39:59 -0500

    printGroceryList

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/GroceryList.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/GroceryList.java
@@ -3,10 +3,16 @@ package com.company;
 import java.util.ArrayList;
 
 public class GroceryList {
-    private int[] myNumbers;
     private ArrayList<String> groceryList = new ArrayList<String>();
 
     public void addGroceryItem(String item) {
         groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
     }
 }
