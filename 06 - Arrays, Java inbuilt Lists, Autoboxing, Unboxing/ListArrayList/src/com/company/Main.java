8970918c13888751cba3dfd12c9ede124bbc99aa
russellkoons <rkoons@gmail.com>
2021-02-12 06:26:20 -0500

    printArray

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
@@ -22,4 +22,11 @@ public class Main {
             baseData[i] = s.nextInt();
         }
     }
-}
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+    }
+ }
