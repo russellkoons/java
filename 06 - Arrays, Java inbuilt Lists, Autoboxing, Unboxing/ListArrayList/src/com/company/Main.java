a9c2f60614fd4ac20c0820e846e631399432de87
russellkoons <rkoons@gmail.com>
2021-02-12 06:24:01 -0500

    main

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner s = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
     public static void main(String[] args) {
-	    
+        System.out.println("Enter 10 integers");
+        getInput();
+        printArray(baseData);
+        resizeArray();
+        System.out.println("Enter 12 integers");
+        getInput();
+        printArray();
     }
 }
