e99ca2d31dc86fd969843e117a73ca7c1a63297d
russellkoons <rkoons@gmail.com>
2021-02-13 09:57:16 -0500

    printInstructions

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/com/company/Main.java
@@ -51,6 +51,17 @@ public class Main {
 //        printArray(baseData);
     }
 
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options");
+        System.out.println("\t 1 - To  print the list of grocery items");
+        System.out.println("\t 2 - To add an item to the list");
+        System.out.println("\t 3 - To modify an item in the list");
+        System.out.println("\t 4 - To remove an item from the list");
+        System.out.println("\t 5 - To search for an item in the list");
+        System.out.println("\t 6 - To quit the application");
+    }
+
 //    private static void getInput() {
 //        for (int i = 0; i < baseData.length; i++) {
 //            baseData[i] = s.nextInt();
