06c464367d58c0522d7fcc00c27afff9b82f1cfd
russellkoons <rkoons@gmail.com>
2021-02-16 05:45:08 -0500

    MobilePhone constructor

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/MobilePhone.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/ListArrayList/src/MobilePhone.java
@@ -3,4 +3,11 @@ import java.util.ArrayList;
 public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    
 }
