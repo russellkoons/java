f5823c701307bd4bd88446dd1d00d24a23f0e965
russellkoons <rkoons@gmail.com>
2021-02-17 05:34:18 -0500

    started autoboxing

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String[] array = new String[10];
+	    int[] intArray = new int[10];
+
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("Russell");
+
+//        ArrayList<int> intArrayList = new ArrayList<int>(); ArrayLists cannot be primitive types
     }
 }
