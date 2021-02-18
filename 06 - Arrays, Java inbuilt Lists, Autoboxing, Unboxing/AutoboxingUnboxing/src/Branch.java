754ebbe0bed34256984d2b5cee9c1001c852a328
russellkoons <rkoons@gmail.com>
2021-02-18 05:30:36 -0500

    fields and constructor for Branch

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/Branch.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/Branch.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Branch {
-    
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name, ArrayList<Customer> customers) {
+        this.name = name;
+        this.customers = customers;
+    }
 }
