6ca0f5dc2e42fb1c5f4fe99c4e2e21c642e1239f
russellkoons <rkoons@gmail.com>
2021-02-18 05:42:35 -0500

    Bank constructor

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/Bank.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/AutoboxingUnboxing/src/Bank.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Bank {
-    
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name, ArrayList<Branch> branches) {
+        this.name = name;
+        this.branches = branches;
+    }
 }
