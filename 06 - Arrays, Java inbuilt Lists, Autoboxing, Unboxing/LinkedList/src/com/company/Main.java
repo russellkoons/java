ed6e91695f8c4f4e7e8834b5df23c9ebb2aefc71
russellkoons <rkoons@gmail.com>
2021-02-19 06:33:11 -0500

    added to LinkedList main

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Customer customer = new Customer("Russell", 4.20);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
     }
 }
