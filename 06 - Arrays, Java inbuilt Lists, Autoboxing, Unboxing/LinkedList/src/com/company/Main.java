4478620522dd6e63f11521a45ad5529d210c1f2b
russellkoons <rkoons@gmail.com>
2021-02-19 06:36:54 -0500

    intList

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Main.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,5 +10,21 @@ public class Main {
         anotherCustomer = customer;
         anotherCustomer.setBalance(12.18);
         System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
     }
 }
