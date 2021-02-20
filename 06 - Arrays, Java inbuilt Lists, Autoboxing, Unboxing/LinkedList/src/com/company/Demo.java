502a448dca855d293578e15fba84c77841055f22
russellkoons <rkoons@gmail.com>
2021-02-20 08:35:03 -0500

    Bbs first linkedlist

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Demo.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Demo.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Atlanta");
+        placesToVisit.add("New York City");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Tokyo");
+    }
 }
