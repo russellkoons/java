d5ce192a53a5b64eee586aa2284ed11664983d0c
russellkoons <rkoons@gmail.com>
2021-02-22 06:22:56 -0500

    goingForward

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Demo.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/com/company/Demo.java
@@ -66,6 +66,7 @@ public class Demo {
     private static void visit(LinkedList cities) {
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
+        boolean goingForward = true;
         ListIterator<String> listIterator = cities.listIterator();
 
         if (cities.isEmpty()) {
@@ -85,6 +86,12 @@ public class Demo {
                     quit = true;
                     break;
                 case 1:
+                    if (!goingForward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
                     if (listIterator.hasNext()) {
                         System.out.println("Now visiting " + listIterator.next());
                     } else {
@@ -92,6 +99,12 @@ public class Demo {
                     }
                     break;
                 case 2:
+                    if (goingForward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
                     if (listIterator.hasPrevious()) {
                         System.out.println("Now visiting " + listIterator.previous());
                     } else {
