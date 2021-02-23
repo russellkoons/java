755aa36715d428637a1ba50b8b159d432c2b5919
russellkoons <rkoons@gmail.com>
2021-02-23 05:41:58 -0500

    Album constructor

--- 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/Album.java
+++ 06 - Arrays, Java inbuilt Lists, Autoboxing, Unboxing/LinkedList/src/Album.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    
 }
