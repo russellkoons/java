fd29cef067444239f25ba69f777d114b740104b2
russellkoons <rkoons@gmail.com>
2021-02-24 06:03:01 -0500

    Filled in DeskPhone methods

--- 07 - Inner and Abstract Classes & Interfaces/Interfaces/src/com/company/DeskPhone.java
+++ 07 - Inner and Abstract Classes & Interfaces/Interfaces/src/com/company/DeskPhone.java
@@ -6,26 +6,34 @@ public class DeskPhone implements ITelephone {
 
     @Override
     public void powerOn() {
-
+        System.out.println("No action taken, desk phone does not have a power button");
     }
 
     @Override
     public void dial(int phoneNumber) {
-
+        System.out.println("Now ringing " + phoneNumber);
     }
 
     @Override
     public void answer() {
-
+        if (isRinging) {
+            System.out.println("Answering the desk phone");
+            isRinging = false;
+        }
     }
 
     @Override
     public boolean callPhone(int phoneNumber) {
-        return false;
+        if (phoneNumber == myNumber) {
+            isRinging = true;
+        } else {
+            isRinging = false;
+        }
+        return isRinging;
     }
 
     @Override
     public boolean isRinging() {
-        return false;
+        return isRinging;
     }
 }
