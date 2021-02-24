32db356ebbf40ef2660f4f33e5c5a7cc48d567cc
russellkoons <rkoons@gmail.com>
2021-02-24 05:57:06 -0500

    ITelephone interface

--- 07 - Inner and Abstract Classes & Interfaces/Interfaces/src/com/company/Gearbox.java
+++ 07 - Inner and Abstract Classes & Interfaces/Interfaces/src/com/company/Gearbox.java
@@ -3,7 +3,7 @@ package com.company;
 public class Gearbox {
     private boolean clutchIsIn;
 
-    public void operateClutch(String inOrOut) {
-        this.clutchIsIn = inOrOut.equalsIgnoreCase("in");
+    public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
     }
 }
