7325353b45fe3315929ebbe837d3fedb085db652
russellkoons <rkoons@gmail.com>
2021-02-25 05:31:59 -0500

    Monster class started

---
+++ 07 - Inner and Abstract Classes & Interfaces/Interfaces/src/Monster.java
@@ -0,0 +1,11 @@
+public class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+}
