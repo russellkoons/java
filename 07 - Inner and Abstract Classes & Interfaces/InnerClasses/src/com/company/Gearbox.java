3c56da5065a72cba002da0213bfb084a8a8b0e9c
russellkoons <rkoons@gmail.com>
2021-02-26 05:37:34 -0500

    Gearbox starter code

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Gearbox.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Gearbox.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Gearbox {
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+    }
 }
