069fae083b3a29b1017952541ecb84614d8fe031
russellkoons <rkoons@gmail.com>
2021-02-26 05:51:30 -0500

    More Gearbox methods

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Gearbox.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Gearbox.java
@@ -6,6 +6,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -14,7 +15,27 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear {
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if ((number > 0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if (newGear >= 0 && newGear < this.gears.size() && this.clutchIsIn) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
