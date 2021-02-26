1f47308d41e8bfd3d435cf67150560e77a6db8ea
russellkoons <rkoons@gmail.com>
2021-02-26 05:44:52 -0500

    what not to do with inner classes

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
         Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        Gearbox.Gear second = mcLaren.new Gear(2, 15.4);
+        Gearbox.Gear third = mcLaren.new Gear(3, 17.8);
+
         System.out.println(first.driveSpeed(1000));
+        System.out.println(second.driveSpeed(1000));
+        System.out.println(third.driveSpeed(1000));
     }
 }
