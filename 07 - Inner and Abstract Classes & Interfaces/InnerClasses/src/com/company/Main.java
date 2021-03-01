c2f3a3108b47abb93a0d03bbac0b2a4f7e8bab5c
russellkoons <rkoons@gmail.com>
2021-03-01 06:25:59 -0500

    added gears to constructor

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
-        mcLaren.addGear(1, 5.3);
-        mcLaren.addGear(2, 10.6);
-        mcLaren.addGear(3, 15.9);
+//        mcLaren.addGear(1, 5.3);
+//        mcLaren.addGear(2, 10.6);
+//        mcLaren.addGear(3, 15.9);
         mcLaren.operateClutch(true);
         mcLaren.changeGear(1);
         mcLaren.operateClutch(false);
@@ -19,9 +19,9 @@ public class Main {
         System.out.println(mcLaren.wheelSpeed(6000));
 
 //        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-////        Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
+//        Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
 //        Gearbox.Gear second = mcLaren.new Gear(2, 15.4);
-////        Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
+//       Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
 //        Gearbox.Gear third = mcLaren.new Gear(3, 17.8);
 //
 //        System.out.println(first.driveSpeed(1000));
