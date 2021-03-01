6469b333c601b308b7aa4ac2ce1e3dd85c89550a
russellkoons <rkoons@gmail.com>
2021-03-01 06:29:09 -0500

    added onClickListener interface

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Button.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Button.java
@@ -19,4 +19,8 @@ public class Button {
     public void onClick() {
         this.onClickListener.onClick(this.title);
     }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
 }
