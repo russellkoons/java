ee3c7aa292c28d2d668674dd00075168677f01cd
russellkoons <rkoons@gmail.com>
2021-03-01 06:31:26 -0500

    ClickListener inner class

--- 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
+++ 07 - Inner and Abstract Classes & Interfaces/InnerClasses/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
     public static void main(String[] args) {
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked!");
+            }
+        }
+
         Gearbox mcLaren = new Gearbox(6);
 //        mcLaren.addGear(1, 5.3);
 //        mcLaren.addGear(2, 10.6);
