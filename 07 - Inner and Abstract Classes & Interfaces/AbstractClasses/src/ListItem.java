b9da7a3e9343400741ee376fa55f17c45c8a7b1f
russellkoons <rkoons@gmail.com>
2021-03-09 05:54:18 -0500

    ListItem

---
+++ 07 - Inner and Abstract Classes & Interfaces/AbstractClasses/src/ListItem.java
@@ -0,0 +1,10 @@
+public abstract class ListItem {
+    protected ListItem rightLink = null;
+    protected ListItem leftLink = null;
+
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+}
--- 07 - Inner and Abstract Classes & Interfaces/AbstractClasses/src/com/company/Dog.java
+++ 07 - Inner and Abstract Classes & Interfaces/AbstractClasses/src/com/company/Dog.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Dog extends Animal {
-    // Adding this comment so I can make a commit on my study day
     public Dog(String name) {
         super(name);
     }
