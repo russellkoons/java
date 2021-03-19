54da3a7a0b0f98e763fc5858dc6ee68c91cb55b9
russellkoons <rkoons@gmail.com>
2021-03-19 05:35:58 -0400

    removeItem

--- 07 - Inner and Abstract Classes & Interfaces/AbstractClasses/src/MyLinkedList.java
+++ 07 - Inner and Abstract Classes & Interfaces/AbstractClasses/src/MyLinkedList.java
@@ -52,6 +52,29 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0) {
+                if (currentItem == this.root) {
+                    this.root = currentItem.next();
+                } else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null) {
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            } else if (comparison < 0) {
+                currentItem = currentItem.next();
+            } else { // comparison > 0
+                return false;
+            }
+        }
         return false;
     }
 
