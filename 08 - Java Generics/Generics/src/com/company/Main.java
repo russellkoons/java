e98e8f23d0656838be61b1930087bbac1a281f48
russellkoons <rkoons@gmail.com>
2021-03-29 04:43:33 -0400

    removed warnings from code

--- 08 - Java Generics/Generics/src/com/company/Main.java
+++ 08 - Java Generics/Generics/src/com/company/Main.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	    ArrayList items = new ArrayList();
+	    ArrayList<Integer> items = new ArrayList<Integer>();
         items.add(1);
         items.add(2);
         items.add(3);
+        // items.add("Russell"); // this crashes the program since String cannot be converted to Integer
         items.add(4);
         items.add(5);
 
         printDoubled(items);
     }
 
-    private static void printDoubled(ArrayList n) {
+    private static void printDoubled(ArrayList<Integer> n) {
         for (Object i : n) {
             System.out.println((Integer) i * 2);
         }
