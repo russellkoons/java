30244ec4b4af095ef17eb3712cc4f3b3471f623f
russellkoons <rkoons@gmail.com>
2021-04-07 06:00:30 -0400

    Team class started;

---
+++ 08 - Java Generics/GenericsPart2/src/com/company/Team.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+    private ArrayList<Player> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player) {
+        if (members.contains(player)) {
+            System.out.println(player.getName() + " is already on the team");
+        }
+    }
+}
