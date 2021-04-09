859c41ec55f9bbe121c257d3d92b87494af533e6
russellkoons <rkoons@gmail.com>
2021-04-09 06:13:28 -0400

    Removed casting from Team

--- 08 - Java Generics/GenericsPart2/src/com/company/Team.java
+++ 08 - Java Generics/GenericsPart2/src/com/company/Team.java
@@ -21,11 +21,11 @@ public class Team<T extends Player> {
 
     public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println(((Player) player).getName() + " is already on the team");
+            System.out.println(player.getName() + " is already on the team");
             return false;
         } else {
             members.add(player);
-            System.out.println(((Player) player).getName() + " picked for team " + this.name);
+            System.out.println(player.getName() + " picked for team " + this.name);
             return true;
         }
     }
