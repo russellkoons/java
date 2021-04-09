502872dbdb6c7e1b4eb79b8c6017f156bbe117cc
russellkoons <rkoons@gmail.com>
2021-04-09 06:21:07 -0400

    matchResults

--- 08 - Java Generics/GenericsPart2/src/com/company/Main.java
+++ 08 - Java Generics/GenericsPart2/src/com/company/Main.java
@@ -22,5 +22,14 @@ public class Main {
         Team<FootballPlayer> melbourne = new Team<>("Melboure");
         FootballPlayer banks = new FootballPlayer("Gordon");
         melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+        adelaideCrows.matchResult(chicagoCubs, 1, 1);
     }
 }
