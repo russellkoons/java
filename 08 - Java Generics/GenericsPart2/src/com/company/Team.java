d0a8d3c09b40adcbc4079fae0f75dc5b5a8e8ae3
russellkoons <rkoons@gmail.com>
2021-04-15 05:55:05 -0400

    compareTo method

--- 08 - Java Generics/GenericsPart2/src/com/company/Team.java
+++ 08 - Java Generics/GenericsPart2/src/com/company/Team.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
     private String name;
     int played = 0;
     int won = 0;
@@ -60,5 +60,16 @@ public class Team<T extends Player> {
 
     public int ranking() {
         return (won * 2) + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        if (this.ranking() > team.ranking()) {
+            return -1;
+        } else if (this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
