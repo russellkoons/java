ee7cba2f16ad55d1c4516290d4cf9fc0d6368e40
russellkoons <rkoons@gmail.com>
2021-04-15 06:12:46 -0400

    added football teams

--- 08 - Java Generics/GenericsChallenge/src/com/company/Main.java
+++ 08 - Java Generics/GenericsChallenge/src/com/company/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
         // ArrayList<Team> teams;
         // Collection.sort(teams); This will sort the teams based on the compareTo method we added
+
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
     }
 }
