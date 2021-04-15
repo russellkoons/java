2d2f95dbc44314c22cc22412848368dfda3b8f99
russellkoons <rkoons@gmail.com>
2021-04-15 05:59:31 -0400

    GenericsChallenge project

---
+++ 08 - Java Generics/GenericsChallenge/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ArrayList<Team> teams;
+        // Collection.sort(teams); This will sort the teams based on the compareTo method we added
+    }
+}
