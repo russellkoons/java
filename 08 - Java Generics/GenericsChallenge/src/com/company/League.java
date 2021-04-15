48d2e13d0bd14173ed51845a1ddfcd2df6961040
russellkoons <rkoons@gmail.com>
2021-04-15 06:09:14 -0400

    league.add

--- 08 - Java Generics/GenericsChallenge/src/com/company/League.java
+++ 08 - Java Generics/GenericsChallenge/src/com/company/League.java
@@ -4,5 +4,18 @@ import java.util.ArrayList;
 
 public class League<T extends Team> {
     public String name;
-    private ArrayList<T> league = new ArrayList<T>();
+    private ArrayList<T> league = new ArrayList<>();
+
+    public League(String name) {
+        this.name = name;
+    }
+
+    public boolean add(T team) {
+        if (league.contains(team)) {
+            return false;
+        }
+
+        league.add(team);
+        return true;
+    }
 }
