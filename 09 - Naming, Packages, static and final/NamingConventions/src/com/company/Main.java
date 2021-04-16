d1478929aebdbb8fbfcdfaad05ef0d929371ca35
russellkoons <rkoons@gmail.com>
2021-04-16 06:01:48 -0400

    naming convention notes

--- 09 - Naming, Packages, static and final/NamingConventions/src/com/company/Main.java
+++ 09 - Naming, Packages, static and final/NamingConventions/src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // packages should be lowercase
+        // Class names use CamelCase
+        // Interface names should also use CamelCase
+        // Method names should be mixedCase and are often verbs
+        // Constants use ALL_UPPER_CASE, declared using the final keyword
+        // Variables use mixedCase, name should be meaningful, no underscores
+        // Type parameters, single character, capital letter
     }
 }
