38e560c98ef4054a7b44af288d274d69043f0955
russellkoons <rkoons@gmail.com>
2021-04-19 06:31:50 -0400

    MyWindow

---
+++ 09 - Naming, Packages, static and final/WinDemo/src/com/example/awtSample/MyWindow.java
@@ -0,0 +1,10 @@
+package com.example.awtSample;
+
+import java.awt.*;
+
+public class MyWindow extends Frame {
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+    }
+}
