9c1dc6cefdb57b373615e3fa13e15556aee78306
russellkoons <rkoons@gmail.com>
2021-04-28 06:03:21 -0400

    Copied Player from interfaces

--- 09 - Naming, Packages, static and final/PackageDemo/src/com/example/game/Player.java
+++ 09 - Naming, Packages, static and final/PackageDemo/src/com/example/game/Player.java
@@ -1,4 +1,74 @@
 package com.example.game;
 
-public class Player {
+import java.util.*;
+
+public class Player implements ISaveable {
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.weapon = "Sword";
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add(this.name);
+        stringList.add("" + this.hitPoints);
+        stringList.add("" + this.strength);
+        stringList.add(this.weapon);
+        return stringList;
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if (list != null && list.size() > 0) {
+            this.name = list.get(0);
+            this.hitPoints = Integer.parseInt(list.get(1));
+            this.strength = Integer.parseInt(list.get(2));
+            this.weapon = list.get(3);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player{name='" + this.name + "', hitPoints=" + this.hitPoints + ", strength=" + this.strength + ", weapon='" + this.weapon + "'}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
